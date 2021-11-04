5f2ef314cb6eceeab20ac39ef2edb49d4d5c93fa
jharenza <jolynnerokita@d3b.center>
2021-11-04 12:35:24 -0400

    rerun with new excel file from jenny

--- analyses/oncoplot/00-create-subset-matrix.R
+++ analyses/oncoplot/00-create-subset-matrix.R
@@ -21,26 +21,16 @@ source(file.path(input_dir, "mutation-colors.R"))
 goi.list <- read_tsv(file.path(input_dir, "goi-mutations"), col_names = "genes")
 
 # https://chopri.box.com/s/pjfwokqko90onbc9zosujtlikdd3r9iq
-hgat <- read_tsv(file.path(data_dir,"ALT_May_2021_JS_plus_v19_histologies.tsv")) %>%
+hgat <- read_tsv(file.path("../add-histologies/output/ALT PBTA oct 2021 (including all plates)-updated-hist-alt.tsv")) %>%
   mutate(Tumor_Sample_Barcode = Kids_First_Biospecimen_ID_DNA) %>%
   filter(short_histology == "HGAT")
 
-tmb <- read_tsv(file.path(data_dir,"pbta-snv-consensus-TMB_intarget.txt"))
+#tmb <- read_tsv(file.path(data_dir,"pbta-snv-consensus-TMB_intarget.txt"))
 
-hgat <- hgat %>%
-  left_join(tmb,by=c("Tumor_Sample_Barcode"))
+#hgat <- hgat %>%
+#  left_join(tmb,by=c("Tumor_Sample_Barcode"))
 
-# select 1 id from multiple RNA 
-hgat_uniq_rna <- hgat %>%
-  group_by(Kids_First_Participant_ID,tumor_descriptor) %>%
-  summarise(Kids_First_Biospecimen_ID_RNA=sample(Kids_First_Biospecimen_ID_RNA,1),
-            Kids_First_Biospecimen_ID_DNA=sample(Kids_First_Biospecimen_ID_DNA,1))
-
-hgat <- arrange(hgat,as.numeric(telomere_ratio)) %>%
-  filter(Kids_First_Biospecimen_ID_RNA %in% hgat_uniq$Kids_First_Biospecimen_ID_RNA) %>%
-  as.data.frame() %>% remove_rownames() 
-
-###read in maf
+###read in maf - we don't have VAF??
 keep_cols <- c("Hugo_Symbol", 
                "Chromosome", 
                "Start_Position", 
@@ -49,36 +39,22 @@ keep_cols <- c("Hugo_Symbol",
                "Tumor_Seq_Allele2", 
                "Variant_Classification", 
                "Variant_Type",
-               "Tumor_Sample_Barcode",
-               "VAF")
+               "Tumor_Sample_Barcode")
 
-consensus <- data.table::fread(file.path(data_dir, "pbta-snv-consensus-mutation.maf.tsv.gz"),
-                      select = keep_cols, data.table = FALSE)
-
-# Hotspots were copied over from OpenPBTA-analysis/analyses/hotspots-detection/results
-hotspots <- data.table::fread(file.path(data_dir,"pbta-snv-scavenged-hotspots.maf.tsv.gz"),
-                     select = keep_cols, data.table = FALSE)
-
-full_maf <- consensus %>%
-  bind_rows(hotspots) %>%
+consensus <- data.table::fread(file.path(data_dir, "snv-consensus-plus-hotspots.maf.tsv.gz"),
+                      select = keep_cols, data.table = FALSE) %>%
   # filter for HGAT
   filter(Tumor_Sample_Barcode %in% hgat$Tumor_Sample_Barcode) %>%
   unique()
 
-##read in cnv copied over from OpenPBTA-analysis/analyses/focal-cn-file-preparation/results
-cnv_autosomes_df <- readr::read_tsv(file.path(data_dir, "consensus_seg_annotated_cn_autosomes.tsv.gz")) %>%
-  left_join(select(hgat,c("Tumor_Sample_Barcode","germline_sex_estimate")),
-            by=c("biospecimen_id"="Tumor_Sample_Barcode")
-  )
-cnv_xy_df <- readr::read_tsv(file.path(data_dir, "consensus_seg_annotated_cn_x_and_y.tsv.gz")) 
-cnv_df <- rbind(cnv_autosomes_df,cnv_xy_df)
-cnv_df <- cnv_df %>%
+##read in cnv copied over from OpenPedCan v9
+cnv_df <- readr::read_tsv(file.path(data_dir, "consensus_wgs_plus_cnvkit_wxs.tsv.gz")) %>%
   mutate(Hugo_Symbol = gene_symbol,
          Tumor_Sample_Barcode = biospecimen_id,
          Variant_Classification = status) %>%
   select(Hugo_Symbol, Tumor_Sample_Barcode, Variant_Classification) %>%
   # mutate loss and amplification to Del and Amp to fit Maftools format
-  dplyr::mutate(Variant_Classification = dplyr::case_when(Variant_Classification == "loss" ~ "Del",
+  dplyr::mutate(Variant_Classification = dplyr::case_when(Variant_Classification == "deep deletion" ~ "Del",
                                                           Variant_Classification == "amplification" ~ "Amp",
                                                           TRUE ~ as.character(Variant_Classification))) %>%
   # only keep Del and Amp calls
@@ -93,7 +69,7 @@ cnv_df <- cnv_df %>%
   as.data.frame()
 
 # complex heatmap
-collapse_snv_dat <- select(full_maf,c(Hugo_Symbol,Tumor_Sample_Barcode,Variant_Classification)) %>%
+collapse_snv_dat <- select(consensus,c(Hugo_Symbol,Tumor_Sample_Barcode,Variant_Classification)) %>%
   unique() %>%
   filter(Variant_Classification %in% names(colors)) %>%
   group_by(Hugo_Symbol,Tumor_Sample_Barcode) %>%
@@ -124,8 +100,8 @@ merged_dat <- merged_dat %>%
 gene_matrix<-reshape2::acast(merged_dat,
                              Hugo_Symbol~Tumor_Sample_Barcode,value.var = "Variant_Classification")
 
-saveRDS(gene_matrix,"hgat_snv_cnv_alt_matrix.RDS")
-write_tsv(hgat,"hgat_subset.tsv")
+saveRDS(gene_matrix,"input/hgat_snv_cnv_alt_matrix.RDS")
+write_tsv(hgat,"input/hgat_subset.tsv")
 
     
     
