2677e5ab5cccd3099931200ce74cd8f591d5d904
runjin326 <47674661+runjin326@users.noreply.github.com>
2022-04-12 14:32:05 -0400

    scriptsa added

---
+++ analyses/07-additional_figures/mut_count_box.R
@@ -0,0 +1,59 @@
+
+library(readr)
+library(tidyverse)
+library(ggpubr)
+
+# define directories
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
+analysis_dir <- file.path(root_dir, "analyses", "07-additional_figures")
+plots_dir <- file.path(analysis_dir, "plots")
+input_dir <- file.path(root_dir, "analyses", "05-oncoplot", "input")
+
+# metadata read in
+metadata <- read_tsv(file.path(root_dir, "analyses", "02-add-histologies", "output",
+                               "stundon_hgat_03312022_updated_hist_alt.tsv")) %>%
+  dplyr::rename(Tumor_Sample_Barcode = Kids_First_Biospecimen_ID_DNA) %>%
+  dplyr::select(Tumor_Sample_Barcode, `alt final`)
+
+# merged mutation matrix read in
+merged_dat <- readRDS(file.path(input_dir, "merged_mut_data.RDS")) %>%
+  dplyr::mutate(Variant_Classification = gsub(",", "", Variant_Classification)) %>%
+  dplyr::filter(Variant_Classification %in% c("Missense_Mutation",
+                                              "Nonsense_Mutation",
+                                              "Frame_Shift_Del",
+                                              "Frame_Shift_Ins",
+                                              "Splice_Site",
+                                              "Translation_Start_Site",
+                                              "Nonstop_Mutation",
+                                              "In_Frame_Del",
+                                              "In_Frame_Ins",
+                                              "Stop_Codon_Ins",
+                                              "Start_Codon_Del",
+                                              "Fusion",
+                                              "Multi_Hit_Fusion",
+                                              "Multi_Hit")) 
+# filter to genes of interest 
+merged_dat_4_genes <- merged_dat %>%
+  dplyr::filter(Hugo_Symbol %in% c("TP53",
+                                   "H3F3A",
+                                   "ATRX",
+                                   "NF1"))
+  
+# generate count dataframe
+count_df_4_genes <- merged_dat_4_genes %>%
+  dplyr::group_by(Tumor_Sample_Barcode) %>%
+  dplyr::mutate(mut_count = sum(count)) %>%
+  dplyr::select(Tumor_Sample_Barcode, mut_count) %>%
+  distinct() %>%
+  dplyr::left_join(metadata) %>%
+  dplyr::filter(mut_count < 10)
+
+# output plots
+pdf(file.path(plots_dir, "mut_count_alt.pdf"))
+p <- ggplot(count_df_4_genes, aes(x =`alt final`, y = mut_count)) +
+  geom_boxplot() + 
+  geom_jitter() + 
+  stat_compare_means(method='t.test') + 
+  
+print(p)
+dev.off()
