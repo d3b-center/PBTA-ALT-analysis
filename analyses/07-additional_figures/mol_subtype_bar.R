db9817e138801dd7ca4f468e746ffb217e024ded
runjin326 <47674661+runjin326@users.noreply.github.com>
2022-04-13 15:36:35 -0400

    plots added

---
+++ analyses/07-additional_figures/mol_subtype_bar.R
@@ -0,0 +1,45 @@
+library(readr)
+library(tidyverse)
+library(ggpubr)
+
+# define directories
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
+analysis_dir <- file.path(root_dir, "analyses", "07-additional_figures")
+plots_dir <- file.path(analysis_dir, "plots")
+data_dir <- file.path(root_dir, "analyses", "02-add-histologies")
+
+# metadata read in
+metadata <- read_tsv(file.path(data_dir,
+                               "output",
+                               "stundon_hgat_03312022_updated_hist_alt.tsv")) %>%
+  dplyr::select(Kids_First_Biospecimen_ID = Kids_First_Biospecimen_ID_DNA, `alt final`)
+
+histology_pbta <- read_tsv(file.path(data_dir,
+                                     "input-v21",
+                                     "pbta-histologies.tsv")) %>%
+  dplyr::select(Kids_First_Biospecimen_ID, molecular_subtype)
+
+# calculate percentage of mutations for different subtypes 
+num_pos <- metadata %>%
+  dplyr::filter(`alt final` == "POS") %>% 
+  nrow()
+num_neg <- metadata %>%
+  dplyr::filter(`alt final` == "NEG") %>% 
+  nrow()
+# calculate the percentage
+combined_df <- metadata %>%
+  left_join(histology_pbta) %>%
+  dplyr::group_by(molecular_subtype) %>%
+  dplyr::mutate(freq = case_when(
+    `alt final` == "POS" ~ length(molecular_subtype) / num_pos,
+    `alt final` == "NEG" ~ length(molecular_subtype) / num_neg))
+
+# output the plot
+pdf(file.path(plots_dir, "mol_subtype_stacked.pdf"))
+p <- ggplot(combined_df, 
+            aes(x=`alt final`, y=freq, fill=molecular_subtype)) + 
+  geom_bar(position="fill", stat="identity")
+print(p)
+dev.off()
+
+
