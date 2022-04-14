c0392b56c0c4476eb897423c93cf2e36a806a6e2
runjin326 <47674661+runjin326@users.noreply.github.com>
2022-04-14 09:33:54 -0400

    plots modified

--- analyses/05-oncoplot/code/01-plot_oncoplot.R
+++ analyses/05-oncoplot/code/01-plot_oncoplot.R
@@ -35,10 +35,13 @@ hgat <- hgat %>%
       tmb < 10 ~ "Normal",
       tmb >= 10 & tmb < 100 ~ "Hypermutant",
       tmb>= 100 ~ "Ultra-hypermutant")
-  )
+  ) %>%
+  dplyr::rename(`Phase of therapy` = tumor_descriptor,
+                Sex = germline_sex_estimate,
+                `Telomere ratio` = telomere_ratio )
 # order columns for plotting
 hgat$`C-circle` <- factor(hgat$`C-circle`, levels = c("POS", "NEG", "Not done"))
-hgat$germline_sex_estimate <- factor(hgat$germline_sex_estimate, levels = c("Male", "Female"))
+hgat$Sex <- factor(hgat$Sex, levels = c("Male", "Female"))
 hgat$TMB <- factor(hgat$TMB, levels = c("Ultra-hypermutant", "Hypermutant", "Normal"))
 
 #subset for what's in the meta file
@@ -48,23 +51,23 @@ setdiff(hgat$Kids_First_Biospecimen_ID_DNA, colnames(gene_matrix))
 ## color for barplot
 col = colors
 names(hgat)
-df = hgat[,c("germline_sex_estimate","tumor_descriptor", "telomere_ratio","C-circle", "TMB")]
+df = hgat[,c("Sex","Phase of therapy", "Telomere ratio","C-circle", "TMB")]
 
-ha = HeatmapAnnotation( name = "annotation", df = hgat[,c("germline_sex_estimate","tumor_descriptor", "telomere_ratio","C-circle", "mutation_status")],
+ha = HeatmapAnnotation( name = "annotation", df = hgat[,c("Sex","Phase of therapy", "Telomere ratio","C-circle", "TMB")],
                        # "TMB"=anno_barplot(hgat$TMB, ylim = c(0,6), gp = gpar(fill = "#CCCCCC80")),
                         col=list(
-                          "germline_sex_estimate" = c("Male" = "#CAE1FF",
+                          "Sex" = c("Male" = "#CAE1FF",
                                                       "Female" = "#FFC1C1"),
-                          "tumor_descriptor" = c("Initial CNS Tumor" = "#7FFFD4",
+                          "Phase of therapy" = c("Initial CNS Tumor" = "#7FFFD4",
                                                  "Progressive" = "#FFFFB5",
                                                  "Progressive Disease Post-Mortem" = "#EEAEEE",
                                                  "Recurrence" = "#ABDEE6",
                                                  "Second Malignancy" = "#CBAACB"),
-                          "telomere_ratio" = colorRamp2(c(0, 1.05, 1.06), c("whitesmoke", "#CAE1FF","dodgerblue4")),
+                          "Telomere ratio" = colorRamp2(c(0, 1.05, 1.06), c("whitesmoke", "#CAE1FF","dodgerblue4")),
                           "C-circle" = c("POS"="dodgerblue4",
                                          "NEG"="whitesmoke",
                                          "Not done" = "gainsboro"),
-                          "mutation_status" = c("Ultra-hypermutant" = "#CAE1FF", 
+                          "TMB" = c("Ultra-hypermutant" = "#CAE1FF", 
                                                 "Hypermutant" = "#FFFFB5", 
                                                 "Normal" = "whitesmoke")),
                       annotation_name_side = "right", annotation_name_gp = gpar(fontsize = 9),
