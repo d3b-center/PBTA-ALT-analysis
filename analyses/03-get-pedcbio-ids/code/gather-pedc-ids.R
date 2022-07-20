67256aaeaca754e4bd9d3eaf48f0287b8ba6615c
jharenza <jolynnerokita@d3b.center>
2022-07-19 20:08:07 -0400

    update pedcbio ids to use alt final

--- analyses/03-get-pedcbio-ids/code/gather-pedc-ids.R
+++ analyses/03-get-pedcbio-ids/code/gather-pedc-ids.R
@@ -14,7 +14,7 @@ meta<- read_tsv(file.path(input_dir,
                          "stundon_hgat_updated_hist_alt.tsv"))
 
 meta %>%
-  dplyr::group_by(phenotype, group) %>%
+  dplyr::group_by(`alt final`, group) %>%
   dplyr::select(Kids_First_Biospecimen_ID_DNA)
 
 hist <- read_tsv(file.path(data_dir,
@@ -38,29 +38,29 @@ pedc <- read_tsv(file.path(analysis_dir,
 meta_ids <- meta %>%
   dplyr::left_join(pedc, by = c("Kids_First_Participant_ID", "Kids_First_Biospecimen_ID_DNA", "Kids_First_Biospecimen_ID_RNA")) %>%
   dplyr::select(Kids_First_Participant_ID, Kids_First_Biospecimen_ID_DNA, Kids_First_Biospecimen_ID_RNA, 
-         sample_id, phenotype, `CCA Sept 2021`, `Sample ID`, group, `TH T/TH N`) %>%
+         sample_id, `alt final`, `CCA Sept 2021`, `Sample ID`, group, `TH T/TH N`) %>%
   dplyr::filter(!is.na(`Sample ID`))
 
 # write case lists
 alt_hgat <- meta_ids %>%
-  dplyr::filter(group == "HGAT" & phenotype == "ALT") %>%
+  dplyr::filter(group == "HGAT" & `alt final` == "POS") %>%
   dplyr::select(`Sample ID`) %>%
   readr::write_tsv(file.path(output_dir,
                              "alt_hgat_ids.txt"), col_names = F)
 
 nonalt_hgat <- meta_ids %>%
-  dplyr::filter(group == "HGAT" & phenotype == "non-ALT") %>%
+  dplyr::filter(group == "HGAT" & `alt final` == "NEG") %>%
   dplyr::select(`Sample ID`) %>%
   readr::write_tsv(file.path(output_dir,
                              "nonalt_hgat_ids.txt"), col_names = F)
 
 alt_all <- meta_ids %>%
-  dplyr::filter(phenotype == "ALT") %>%
+  dplyr::filter(`alt final` == "POS") %>%
   dplyr::select(`Sample ID`) %>%
   readr::write_tsv(file.path(output_dir,"alt_ids.txt"), col_names = F)
 
 nonalt_all <- meta_ids %>%
-  dplyr::filter(phenotype == "non-ALT") %>%
+  dplyr::filter(`alt final` == "NEG") %>%
   dplyr::select(`Sample ID`) %>%
   readr::write_tsv(file.path(output_dir, "nonalt_ids.txt"), col_names = F)
 
@@ -82,6 +82,3 @@ hgat_small_telhunt <- meta_ids %>%
 bind_rows(nonalt_hgat, hgat_small_telhunt) %>%
   distinct() %>% 
   readr::write_tsv(file.path(output_dir, "nonalt_hgat_w_hgat_telhunt_less107_ids.txt"), col_names = F)
-
- names(meta)
- 
