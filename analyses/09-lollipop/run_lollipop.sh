a7692825a65bba3b288febe753b3fe99fc8baec4
rjcorb <corbettr@chop.edu>
2022-11-02 16:24:59 -0400

    created command line args for oncoKB token

--- analyses/09-lollipop/run_lollipop.sh
+++ analyses/09-lollipop/run_lollipop.sh
@@ -3,5 +3,17 @@
 set -e
 set -o pipefail
 
-# lollipop plots
-Rscript -e "rmarkdown::render('code/01-create-lollipop.Rmd')"
+# OncoKB access token required to run module; if not provided then exit
+if [ "$ONCOKB" = "" ]; then
+    echo 'Error: An Oncokb access token is required to run this module in full. Please provide token before running analysis module in the format ONCOKB=<TOKEN> bash run_lollipop.sh'
+    exit 1
+fi
+
+# create subset MAF
+Rscript -e "rmarkdown::render('code/01-create-subset-maf.Rmd')"
+
+# Run oncokb MAF annotation: 
+ONCOKB=$ONCOKB bash code/02-run-oncokb-annotation.sh
+
+# make lollipop plots
+Rscript -e "rmarkdown::render('code/03-create-lollipop.Rmd')"
